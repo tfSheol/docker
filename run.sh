15e258e37d607155c3e089f45c91889794047f4b
sheol <tf.sheol@gmail.com>
2018-11-22 14:12:20 +0100

    docker build

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -e
 rm -rf build
 mkdir -p build
 
-docker rm "docker-build"
+#docker rm "docker-build"
 docker run -it --privileged --name "docker-build" "docker/docker-ce"
 docker cp "docker-build":/opt/app/docker-ce/components/packaging/static/build/win/docker/ build
 docker rm "docker-build"
